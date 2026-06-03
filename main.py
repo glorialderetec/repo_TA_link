2930b2040e644f719ec81732d6cdae280320039e
belenzarich <bzarich@udesa.edu.ar>
2026-06-03 15:54:52 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,90 @@
+# MAIN
+from src.datos import (cargar_dataset, validar_dataset)
+from src.interfaz import(mostrar_menu, pedir_opcion)
+from src.perfiles import Cliente
+from src.metricas import obtener_estadisticas_generales
+try:
+    df = cargar_dataset()
+    df_valido = validar_dataset(df)
+
+except Exception as e:
+    print(e)
+    
+    quit() #SUPER A CHEQUEAR
+
+def pedir_id_cliente(df):
+    '''
+    funcion que pide un id de cliente y lo valida
+
+    Parameters
+    ----------
+    df : DataFrame
+        DataFrame con los datos del dataset ya validados.
+
+    Returns
+    -------
+    id_cliente : int
+        el id del cliente pedido ya validado.
+
+    '''
+
+    while True:
+
+        try:
+
+            id_cliente = int(input("Ingrese ID del cliente: "))
+
+            if id_cliente in df["id"].values:
+                return id_cliente
+
+            print("Error: no existe un cliente con ese ID")
+
+        except ValueError:
+
+            print("Error: debe ingresar un número")
+
+while True:
+    mostrar_menu()
+    opcion = pedir_opcion()
+    
+    if opcion == 1:
+
+        id_cliente = pedir_id_cliente(df_valido)
+
+        fila = df_valido[df_valido["id"] == id_cliente]
+
+        cliente = Cliente(
+            fila["id"].iloc[0],
+            fila["age"].iloc[0],
+            fila["income"].iloc[0],
+            fila["purchase_amount"].iloc[0],
+            fila["promotion_usage"].iloc[0],
+            fila["satisfaction_score"].iloc[0]
+        )
+
+        print("\nPerfil del cliente:")
+        print(cliente.clasificar_perfil())
+
+    elif opcion == 2:
+
+        df_metricas = obtener_estadisticas_generales(df_valido)
+        
+        df_metricas.head()
+
+    elif opcion == 3:
+
+        print("Comparando segmentos...")
+
+    elif opcion == 4:
+
+        print("Mostrando gráficos...")
+
+    elif opcion == 5:
+
+        print("Generando recomendaciones...")
+
+    elif opcion == 6:
+
+        print("Programa finalizado.")
+        break
+
