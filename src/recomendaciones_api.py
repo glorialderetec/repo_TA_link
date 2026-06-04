302f5024db5e71985ec8d465ef5f25d05d06e203
mariaprimbas <primbasmaria@gmail.com>
2026-06-04 14:15:26 -0300

    Update recomendaciones_api.py

--- src/recomendaciones_api.py
+++ src/recomendaciones_api.py
@@ -1,1 +1,3 @@
-
+def generar_recomendacion_api(cliente):
+  prompt= (f"Cliente ID: {cliente.id},Edad: {cliente.age}, Ingresos: {cliente.income}, Frecuencia de compra: {cliente.purchase_frequency}, Monto de compra: {cliente.purchase_amount}, Satisfacción: {cliente.satisfaction_score}, Score: {cliente.obtener_score()}, Perfil: {cliente.clasificar_perfil()}, Recomendación base: {cliente.generar_recomendacion()}. Generá una recomendación de marketing clara y breve para este cliente. ")
+  #aca llamar a la api y pasarle el prompt
