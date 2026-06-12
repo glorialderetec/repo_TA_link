8f191215e4c0ebc9443bc34928438d7ea1d040cb
mariaprimbas <primbasmaria@gmail.com>
2026-06-12 12:23:44 -0300

    Update interfaz.py

--- src/interfaz.py
+++ src/interfaz.py
@@ -11,7 +11,7 @@ def mostrar_menu():
 def pedir_opcion():
     '''
     Función que solicita al usuario una opción del menú y valida
-    que sea un número entero entre 1 y 7.
+    que sea un número entero entre 1 y 8.
 
     Returns
     -------
@@ -22,9 +22,9 @@ def pedir_opcion():
     while True:
 
         try:
-            opcion = int(input("Seleccione una opción (1-7): "))
-            if opcion < 1 or opcion > 7:
-              print("Error: debe ingresar una opción entre 1 y 7.")
+            opcion = int(input("Seleccione una opción (1-8): "))
+            if opcion < 1 or opcion > 8:
+              print("Error: debe ingresar una opción entre 1 y 8.")
               continue
 
             return opcion
